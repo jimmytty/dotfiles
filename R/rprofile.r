6ed66fb7c7218b0f5b9f4a4515e1e1af5033208d
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2014-05-01 09:29:42 -0300

    * new file: R/rprofile.r

---
+++ R/rprofile.r
@@ -0,0 +1,11 @@
+.First <- function() {
+  options(browser   = "browser")
+  options(pdfviewer = "xpdf"   )
+  options(width     = 120      )$STERM
+}
+
+.Last <- function() {}
+
+# Local Variables:
+# mode: r
+# End:
