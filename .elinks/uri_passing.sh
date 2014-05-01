8bc7051ec7de68ea6d9b9ea3a4ea7c201863f16d
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2014-05-01 09:29:43 -0300

    * new file: .elinks/uri_passing.sh

---
+++ .elinks/uri_passing.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+logfile="$HOME/var/log/elinks.log"
+#exec 1>> ${logfile} 2>&1
+
+DISPLAY=:1.0
+read date time <<< $(date +%F\ %T)
+
+outdir="$HOME/work"
+func_name=$1
+uri=$2
+filename=$($HOME/.elinks/uri2filename.pl $uri)
+
+html2org () {
+    xsh_src=$(cat <<'EOF'
+quiet;
+nodebug;
+recovering 1;
+encoding 'utf-8';
+query-encoding 'utf-8';
+foreach my $anchor_node in //node()[@id or @name] {
+    cd $anchor_node;
+    if ( local-name() = 'html' ) { next; }
+    my $anchor_name;
+    if ( @id ) { $anchor_name = @id; }
+    elsif ( @name ) { $anchor_name = @name; }
+    my $orgid = concat('<<','#',$anchor_name,'>>');
+    insert text $orgid before .;
+}
+print normalize-space(xsh:serialize(/));
+EOF
+)
+    pandoc_file="$outdir/pandoc_html2org.org"
+    pandoc_cmd=$(cat <<'EOF'
+pandoc \
+    --no-wrap \
+    --base-header-level=2 \
+    --self-contained \
+    --reference-links \
+    --from=html \
+    --to=org
+EOF
+)
+    (   echo "* $uri";
+        xsh \
+            --no-validation \
+            --html \
+            --input "$filename" \
+            --command "$xsh_src" |
+        $pandoc_cmd
+    ) &>> $pandoc_file
+
+    exit 0
+}
+
+$func_name
