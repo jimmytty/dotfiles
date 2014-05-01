128d882cdecd46a4b114ca81ff9b73609b7abb21
Ronaldo Ferreira de Lima <jimmy.tty@gmail.com>
2014-05-01 09:29:43 -0300

    * new file: .dzil/profiles/default/Module.pm

---
+++ .dzil/profiles/default/Module.pm
@@ -0,0 +1,58 @@
+package {{$name}};
+
+use common::sense;
+use charnames q(:full);
+use English qw[-no_match_vars];
+no autovivification;
+use Moo;
+
+no Moo;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+{{$name}} - ...
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 OPTIONS
+
+=head1 ERRORS
+
+=head1 DIAGNOSTICS
+
+=head1 EXAMPLES
+
+=head1 ENVIRONMENT
+
+=head1 FILES
+
+=head1 CAVEATS
+
+=head1 BUGS
+
+=head1 RESTRICTIONS
+
+=head1 NOTES
+
+=head1 AUTHOR
+
+Ronaldo Ferreira de Lima aka jimmy <jimmy at gmail>.
+
+=head1 HISTORY
+
+=head1 COPYRIGHT AND LICENSE
+
+=head1 SEE ALSO
+
+=cut
